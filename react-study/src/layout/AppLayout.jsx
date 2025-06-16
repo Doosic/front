5c450df8ad09a0f2cf4424e32ea345a6a5ad8f75
DoosicEE <strou73@naver.com>
2025-06-16 22:13:51 +0900

    add: tailwind css

---
+++ react-study/src/layout/AppLayout.jsx
@@ -0,0 +1,21 @@
+import { Outlet, Link } from "react-router-dom";
+
+const AppLayout = () => {
+
+    return (
+        <div>
+            <header className="flex px-2 py-4 space-x-4 h-20 items-center">
+              <p className="text-3xl">React-study</p>
+              <Link to="/state">useState</Link>
+              <Link to="/ref">useRef</Link>
+              <Link to="/effect">useEffect</Link>
+            </header>
+
+          <div className="px-2 py-4">
+            <Outlet />
+          </div>
+        </div>
+    )
+}
+
+export default AppLayout
